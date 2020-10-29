b81506ba0ac4c94be219b758eaf9c2e717c794b5
Liana <lianamkhitaryan37@gmail.com>
2020-10-29 18:42:52 +0400

    Initial commit , Primitive data types in java

---
+++ Homework1/src/Lesson1.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+import static java.lang.StrictMath.abs;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        byte t = 127;
+        byte elem = (byte) 129;
+        System.out.println(elem);
+        System.out.println(abs(elem)==t);
+    }
+}
